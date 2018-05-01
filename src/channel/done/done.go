bab3e3757324864d700c6573f0e4af0678257147
958900zh <zxdong_55@163.com>
2018-05-01 23:52:24 +0800

    Add channel

--- src/channel/done/done.go
+++ src/channel/done/done.go
@@ -2,49 +2,54 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-func doWork(i int, c chan int, done chan bool) {
-	for n := range c {
-		fmt.Printf("worker %d recieved %c\n", i, n)
-		done <- true
+func doWork(id int,
+	w worker) {
+	for n := range w.in {
+		fmt.Printf("Worker %d received %c\n",
+			id, n)
+		w.done()
 	}
-}
-
-func createWorker(i int) worker {
-	w := worker{
-		in:   make(chan int),
-		done: make(chan bool),
-	}
-
-	go doWork(i, w.in, w.done)
-
-	return w
 }
 
 type worker struct {
 	in   chan int
-	done chan bool
+	done func()
+}
+
+func createWorker(
+	id int, wg *sync.WaitGroup) worker {
+	w := worker{
+		in: make(chan int),
+		done: func() {
+			wg.Done()
+		},
+	}
+	go doWork(id, w)
+	return w
 }
 
 func chanDemo() {
+	var wg sync.WaitGroup
+
 	var workers [10]worker
 	for i := 0; i < 10; i++ {
-		workers[i] = createWorker(i)
+		workers[i] = createWorker(i, &wg)
 	}
 
-	for i := 0; i < 10; i++ {
-		workers[i].in <- 'a' + i
-		<-workers[i].done
+	wg.Add(20)
+	for i, worker := range workers {
+		worker.in <- 'a' + i
+	}
+	for i, worker := range workers {
+		worker.in <- 'A' + i
 	}
 
-	for i := 0; i < 10; i++ {
-		workers[i].in <- 'A' + i
-		<-workers[i].done
-	}
+	wg.Wait()
 }
 
 func main() {
 	chanDemo()
-
 }
